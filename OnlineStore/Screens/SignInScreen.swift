fa9399bd9433305494c54ad282b29430695da75b
Sushant Suwal <sushantdsuwal@gmail.com>
2024-02-29 21:15:09 +0545

    feat(screen): loginscreen

--- OnlineStore/Screens/SignInScreen.swift
+++ OnlineStore/Screens/SignInScreen.swift
@@ -8,11 +8,68 @@
 import SwiftUI
 
 struct SignInScreen: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
     var body: some View {
-        Image(systemName: "globe")
-            .imageScale(.large)
-            .foregroundStyle(.tint)
-        Text("Hello, world!")
+        VStack {
+            IntroHeaderView(
+                title: "Welcome to Online Store",
+                subTitle: "Sign in to continue"
+            )
+            
+            SignInInputView(email: $email, password: $password)
+                .padding(.top, 20)
+        }
+        .padding()
+    }
+}
+
+
+struct SignInInputView: View {
+    @Binding var email: String
+    @Binding var password: String
+    
+    var body: some View {
+        VStack(spacing: 15) {
+            HStack {
+                Image(systemName: "envelope")
+                    .frame(width: 20, height: 20)
+                    .padding(.leading, 10)
+                
+                TextField("Email", text: $email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.horizontal, 10)
+                    .autocapitalization(.none)
+            }
+            
+            HStack {
+                Image(systemName: "lock")
+                    .frame(width: 20, height: 20)
+                    .padding(.leading, 10)
+                
+                SecureField("Password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.horizontal, 10)
+            }
+        }
+    }
+}
+
+
+struct IntroHeaderView: View {
+    let title: String;
+    let subTitle: String;
+    
+    var body: some View {
+        VStack {
+            Image(.logo)
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text(title)
+                .padding([.bottom, .top], 5)
+            Text(subTitle).fontWeight(.light)
+        }
     }
 }
 
