419f6624f56a94ea7b5e2756bd920a0338b53162
Sushant Suwal <sushantdsuwal@gmail.com>
2024-02-29 15:47:04 +0545

    Initial Commit

---
+++ OnlineStore/OnlineStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineStoreApp.swift
+//  OnlineStore
+//
+//  Created by Sushant Suwal on 29/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct OnlineStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
