2a6381b6dd4b2679f5563c8243e7c790eefe5f1f
Sushant Suwal <sushantdsuwal@gmail.com>
2024-02-29 15:54:57 +0545

    initilize app

--- OnlineStore/OnlineStoreApp.swift
+++ OnlineStore/OnlineStoreApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct OnlineStoreApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SignInScreen()
         }
     }
 }
---
+++ OnlineStore/Screens/SignInScreen.swift
@@ -0,0 +1,21 @@
+//
+//  SignInScreen.swift
+//  OnlineStore
+//
+//  Created by Sushant Suwal on 29/02/2024.
+//
+
+import SwiftUI
+
+struct SignInScreen: View {
+    var body: some View {
+        Image(systemName: "globe")
+            .imageScale(.large)
+            .foregroundStyle(.tint)
+        Text("Hello, world!")
+    }
+}
+
+#Preview {
+    SignInScreen()
+}
