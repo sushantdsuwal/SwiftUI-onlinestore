04e1afd2d7f083eb63ab2e25275022911763f91b
Sushant Suwal <sushantdsuwal@gmail.com>
2024-03-04 23:48:23 +0545

    fake view modal created for categories

--- OnlineStore/Views/Home/Models/Category.swift
+++ OnlineStore/Views/Home/Models/Category.swift
@@ -13,3 +13,28 @@ struct Category:Identifiable {
     let id: Int
     let iconName: String
 }
+
+
+class CategoryViewModel: ObservableObject {
+    @Published var categories = [Category]()
+    
+    init() {
+        fetchCategories()
+    }
+    
+ 
+    func fetchCategories() {
+        let fakeCategories: [Category] = [
+            Category(name: "Men's Clothing", id: 0, iconName: "figure.hiking"),
+            Category(name: "Women's Clothing", id: 1, iconName: "person.fill"),
+            Category(name: "Electronics Gagets", id: 2, iconName: "tv.fill"),
+            Category(name: "Home & Kitchen", id: 3, iconName: "house.fill"),
+            Category(name: "Beauty & Health", id: 4, iconName: "heart.fill"),
+            Category(name: "Sports & Outdoors", id: 5, iconName: "sportscourt.fill"),
+            Category(name: "Toys & Games", id: 6, iconName: "gamecontroller.fill"),
+            Category(name: "Books Library", id: 7, iconName: "book.fill")
+        ]
+        categories = fakeCategories;
+    }
+    
+}
--- OnlineStore/Views/Home/Screens/HomeScreen.swift
+++ OnlineStore/Views/Home/Screens/HomeScreen.swift
@@ -10,17 +10,6 @@ import SwiftUI
 struct HomeScreen: View {
     @State private var searchProductText: String = ""
     
-    let fakeCategories: [Category] = [
-        Category(name: "Men's Clothing", id: 0, iconName: "figure.hiking"),
-        Category(name: "Women's Clothing", id: 1, iconName: "person.fill"),
-        Category(name: "Electronics Gagets", id: 2, iconName: "tv.fill"),
-        Category(name: "Home & Kitchen", id: 3, iconName: "house.fill"),
-        Category(name: "Beauty & Health", id: 4, iconName: "heart.fill"),
-        Category(name: "Sports & Outdoors", id: 5, iconName: "sportscourt.fill"),
-        Category(name: "Toys & Games", id: 6, iconName: "gamecontroller.fill"),
-        Category(name: "Books Library", id: 7, iconName: "book.fill")
-    ]
-    
     var body: some View {
         SearchHeader(searchProductText: $searchProductText)
             .padding([.leading, .trailing], 20)
@@ -29,8 +18,8 @@ struct HomeScreen: View {
         
         PromotionCarouselView()
         
-        CategoryView(fakeCategories: fakeCategories)
-        
+        CategoryView(fakeCategories: CategoryViewModel().categories)
+
         Spacer()
     }
 }
